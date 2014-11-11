18f9f52715bff9ff6f30ba09f47656bc6653cf58
jujoo <wnfks93@gmail.com>
2014-11-11 19:13:18 +0900

    json수정함

---
+++ js/mixitup.js
@@ -0,0 +1,38 @@
+// $(function(){
+// 	// Instantiate MixItUp:
+// 	$('#Container').mixItUp();
+
+// 	$(".mix" ).children('div').css( "opacity", 0);
+// 	$( ".mix" )
+//       .mouseenter(function() {
+//         $( this ).children('div').stop().animate({ opacity: 1 }, 500);
+//     })
+//     .mouseleave(function() {
+//         $( this ).children('div').stop().animate({ opacity: 0 }, 500);
+//     });
+// });
+$(function(){
+    $('#Container').mixItUp();
+
+    $.getJSON("data/WORKS.json", function(json) {
+        $.each(json.result.V, function(i, item) {
+            var itemLi = $("<li/>", {
+                "class": "mix VISUAL",
+                "data-name": item.workTitle,
+                "data-part": "V",
+                "data-id": item.WORKID
+            });
+            var thumbSrc = "data/img/V/" + item.WORKID + ".jpg";
+
+            $("<div/>", {
+                "class": "active",
+                html: "<span class=title>" + item.workTitle + "</span><span class=desc>" + item.workDesc + "</span><div class='category V'></div>"
+            }).appendTo(itemLi);
+            $("<div/>", { "class": "cover" }).appendTo(itemLi);
+            $("<img/>", { "src": thumbSrc }).appendTo(itemLi);
+
+            WORKS.append(itemLi);
+        });
+    });
+
+});
